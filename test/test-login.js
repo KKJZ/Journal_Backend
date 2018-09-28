e844b027d6c11ae2ee14a7374d3a56897b4b0da6
Kyle Stockmann <kyle_stockmann@yahoo.com>
2018-09-28 14:41:47 -0700

    tests done

--- test/test-login.js
+++ test/test-login.js
@@ -16,6 +16,15 @@ const {TEST_DATABASE_URL} = require('../.config');
 
 chai.use(chaiHttp);
 
+//make a user
+function generateUserData () {
+	let data = {
+		userName: faker.internet.userName(),
+		password: faker.internet.password()
+	}
+	return data
+}
+
 //tear down db
 function tearDownDb () {
 	console.warn('Deleting Test Database');
@@ -35,10 +44,7 @@ describe('Journal Back-end, login endpoint', function() {
 	//POST
 	describe('POST log a user in', function() {
 		it('Should return a token for a already made user', function() {
-			let newUser = {
-				userName: faker.internet.userName(),
-				password: faker.internet.password()
-			};
+			let newUser = generateUserData();
 			return chai.request(app).post('/register').send(newUser)
 			.then(function(res) {
 				expect(res.body).to.have.key('token');
@@ -54,4 +60,25 @@ describe('Journal Back-end, login endpoint', function() {
 		})
 	})
 	//REFRESH
+	describe('POST refresh, takes a token and returns a token', function() {
+		it("Should return a token on refresh", function() {
+			let newUser = generateUserData();
+			let token;
+			return chai.request(app).post('/register').send(newUser)
+			.then(function(res) {
+				expect(res.body).to.have.key('token');
+				expect(res.body.token).to.not.be.null;
+				token = res.body.token
+			})
+			.then(function() {
+				return chai.request(app).post('/login/refresh').set("Authorization", "Bearer "+ token)
+			})
+			.then(function(_res) {
+				expect(_res).to.have.status(200);
+				expect(_res.body).to.have.key('token');
+				expect(_res.token).to.not.be.null;
+			})
+		})		
+	})
+	//may want to add a test to make sure tokens have the right info stored in them
 })
